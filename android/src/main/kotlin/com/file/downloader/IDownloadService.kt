727b4c28630c245488a8c809146a89e185e2603d
Omar <omardevelop19@gmail.com>
2025-06-17 07:38:16 +0300

    commit

--- android/src/main/kotlin/com/file/downloader/IDownloadService.kt
+++ android/src/main/kotlin/com/file/downloader/IDownloadService.kt
@@ -128,7 +128,11 @@ abstract class IDownloadService : Service() {
                         if (result != null) {
                             if (!result.isDone) {
                                 result.cancel(true)
-                                delete(url)
+                                val thread = Thread {
+                                    delete(url)
+                                }
+                                thread.start()
+                                thread.join() // Waits for thread to finish
                                 notifyCanceled(url)
                                 sendCanceled(url)
                                 checkToStopService()
